200eb5f018b3ada97fd3130493c0cafe3e1338ad
Peter Trencansky <peter.trencansky@bait.sk>
2022-05-02 16:01:49 +0200

    Clockify advance

--- load_clokify.php
+++ load_clokify.php
@@ -17,4 +17,24 @@ $params = array();
 
 echo json_encode($user);
 $entries = $client->get("workspaces/".$user->activeWorkspace()."/user/".$user->id()."/time-entries");
-echo json_encode($entries);
+$entriesToAdd = array();
+foreach ($entries as $entry){
+    while(date("Y-m-d", strtotime($entry["timeInterval"]["start"])) < date("Y-m-d", strtotime($entry["timeInterval"]["end"]))){
+        $newEntry = array();
+        $newEntry["timeInterval"]=array();
+        $newEntry["timeInterval"]["start"]=date("Y-m-d", strtotime($entry["timeInterval"]["start"]));
+        $date = new DateTime($entry["timeInterval"]["start"]);
+        $date->setTime(23, 59);
+        $newEntry["timeInterval"]["end"]=$date->format(DateTimeInterface::ATOM);
+        $newEntry["description"]=$entry["description"];
+        $entriesToAdd[]=$newEntry;
+        $date -> add(new DateInterval("1 day"));
+        $date->setTime(0, 0);
+        $entry["timeInterval"]["start"] = $date->format(DateTimeInterface::ATOM);
+    }
+}
+$entriesByDate = array();
+foreach ($entries as $entry){
+    $entriesByDate[date("Y-m-d", strtotime($entry["timeInterval"]["start"]))][]=$entry;
+}
+echo json_encode($entriesByDate, JSON_PRETTY_PRINT);
